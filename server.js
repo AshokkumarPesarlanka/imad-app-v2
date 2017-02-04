eac9db17e41a36603b18fc3812779d8ead6150fd
Ashokkumar Pesarlanka <ashokkumar.pesarlanka@gmail.com>
2017-02-04 10:27:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,16 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/Article-one',function(req,res){
-    res.sendFile('This is Artcie-one');
+    res.send('This is Artcie-one');
 });
 
+app.get('/Article-two',function(req,res){
+    res.send('This is Artcie-two');
+});
 
+app.get('/Article-three',function(req,res){
+    res.send('This is Artcie-three');
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
