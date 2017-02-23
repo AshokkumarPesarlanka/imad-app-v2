4af33db6577e22c56564e2bde58743bc33ea0ee8
Ashokkumar Pesarlanka <ashokkumar.pesarlanka@gmail.com>
2017-02-23 22:38:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,21 @@ app.get('/test-db',function(req,res){
     
 });
 
+app.get('/:articleName',function(req,res){
+    
+   pool.query("select * from article where title='"+req.params.articleName+"'",function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          if(result.rows.length==0){
+              res.status(404).send("Article Not Found");
+          }else{
+              res.send(JSON.stringify(result.rows));
+          }
+      } 
+   }); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
