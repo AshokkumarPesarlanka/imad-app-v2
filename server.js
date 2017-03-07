d3c184fa54d57e59b83d7432beff33d89b656963
Ashokkumar Pesarlanka <ashokkumar.pesarlanka@gmail.com>
2017-03-07 22:13:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config={
     user:'ashokkumarpesarlanka',
     database:'ashokkumarpesarlanka',
@@ -66,9 +67,12 @@ app.get('/:articleName',function(req,res){
       } 
    }); 
 });
-
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
 app.get('/hash/:input',function(req,res){
-   var hashstring=hash(req.params.input);
+   var hashstring=hash(req.params.input,'this-is-some-random-string');
    res.send(hashstring);
 });
 
